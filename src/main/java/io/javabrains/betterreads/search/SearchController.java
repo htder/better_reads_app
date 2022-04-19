e63ddac59151874c82e8440e644ba9211e329a3b
Henry Derbyshire <henry@htderbyshire.com>
2022-04-19 10:11:40 +0100

    created SearchResult and SearchResultBook classes, add functionality to search controller to retreive data from open library api with a query

--- src/main/java/io/javabrains/betterreads/search/SearchController.java
+++ src/main/java/io/javabrains/betterreads/search/SearchController.java
@@ -1,9 +1,15 @@
 package io.javabrains.betterreads.search;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class SearchController {
@@ -11,17 +17,33 @@ public class SearchController {
     private final WebClient webClient;
 
     public SearchController(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder
-                .baseUrl("https://openlibrary.org/search.json")
-                .build();
+        this.webClient =
+                webClientBuilder
+                        .exchangeStrategies(
+                                ExchangeStrategies.builder()
+                                        .codecs(configurer ->
+                                                configurer
+                                                        .defaultCodecs()
+                                                        .maxInMemorySize(16 * 1024 * 1024))
+                                        .build())
+                        .baseUrl("https://openlibrary.org/search.json")
+                        .build();
     }
 
-    public String getSearchResults(@RequestParam String query) {
-        Mono<String> foo = this.webClient.get()
-                .uri("?q={query}", query)
-                .retrieve()
-                .bodyToMono(String.class);
-        String result = foo.block();
+    @GetMapping(value = "/search")
+    public String getSearchResults(@RequestParam String query, Model model) {
+        Mono<SearchResult> resultsMono =
+                this.webClient
+                        .get()
+                        .uri("?q={query}", query)
+                        .retrieve()
+                        .bodyToMono(SearchResult.class);
+        SearchResult result = resultsMono.block();
+        List<SearchResultBook> books = result.getDocs()
+                                    .stream()
+                                    .limit(10)
+                                    .collect(Collectors.toList());
+        model.addAttribute("searchResult", books);
         return "search";
     }
 }
