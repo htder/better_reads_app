ad98e4ea299909bb33364f349f6962c5b4e28ac8
Henry Derbyshire <henry@htderbyshire.com>
2022-04-19 19:16:00 +0100

    created home page controller and view

--- src/main/java/io/javabrains/betterreads/user/BooksByUserRepository.java
+++ src/main/java/io/javabrains/betterreads/user/BooksByUserRepository.java
@@ -1,12 +1,13 @@
 package io.javabrains.betterreads.user;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
-import java.awt.print.Pageable;
+
 
 public interface BooksByUserRepository extends CassandraRepository<BooksByUser, String> {
 
-    Slice<BooksByUser> findAllBy(String id, Pageable pageable);
+    Slice<BooksByUser> findAllById(String id, Pageable pageable);
 
 }
